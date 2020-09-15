4bb3aeb96965bcd197f8f5d4ec0503089b924d09
Jean Cheikh Ndiaye <jeancheikh@gmail.com>
2020-09-15 16:25:11 -0400

    Update db.js

--- models/db.js
+++ models/db.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+
 let dbURL = "mongodb://localhost/AfriteachDB";
 //if (process.env.NODE_ENV === "production") {
   dbURL = process.env.MONGODB_URL;
