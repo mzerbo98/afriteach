145fcd30d26986f26894b50477fe5d0140251b98
Jean Cheikh Ndiaye <jeancheikh@gmail.com>
2020-09-20 10:24:31 -0400

    Update db.js

--- models/db.js
+++ models/db.js
@@ -5,29 +5,22 @@ dotenv.config();
 
 
 let dbURL = "mongodb://localhost/AfriteachDB";
-//if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   dbURL = process.env.MONGODB_URL;
-//}
+}
 
-mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 mongoose.connection.on("connected", () => {
   console.log(`Mongoose connected to ${dbURL}`);
 });
 
-// mongoose.connection.on("error", (err) => {
-//   console.log("Mongoose connection error: ", err);
-// });
-
-// mongoose.connection.on("disconnected", () => {
-//   console.log("Mongoose disconnected");
-// });
 const gracefulShutdown = (msg, callback) => {
   mongoose.connection.close(() => {
-    // console.log(`Mongoose disconnected through ${msg}`);
     callback();
   });
 };
+
 // from nodemon restarts
 process.once("SIGUSR2", () => {
   gracefulShutdown("nodemon restart", () => {
