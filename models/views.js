e061b9ba69f069001d18464521e1d3f59a709261
MZ <mzerbo98@gmail.com>
2020-09-21 14:20:15 +0000

    course stream and view count

---
+++ models/views.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const viewSchema = new mongoose.Schema(
+    {
+        date: {
+            type: Date,
+            default: Date.now
+        },
+        viewer: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        },
+        course: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Course'
+        },
+        ip_address: {
+            type: String,
+            trim: true,
+        }
+    }
+);
+
+mongoose.model('View', viewSchema);
