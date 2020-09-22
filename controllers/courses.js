66fe6ebe53665a105d2871df1b56f1dea72caa8a
MZ <mzerbo98@gmail.com>
2020-09-22 07:54:28 +0000

    corrected codacy errors

--- controllers/courses.js
+++ controllers/courses.js
@@ -20,8 +20,10 @@ const stream = (req, res) => {
     const courseId = req.params.id;
     Course.findById(courseId, function(err, course){
         if (err) {
-            console.log("course stream error", err);
-            res.status(404).send("Course stream error");
+            res.status(404).json({
+                message: "Course stream error",
+                error: err
+            });
         } else {
             const token = req.query.token;
             const ip = req.ip;
@@ -29,7 +31,6 @@ const stream = (req, res) => {
             if (token) {
                 jwt.verify(token, process.env.JWT_TOKEN || "secret", (err, user) => {
                     if (err) {
-                        console.log("token error");
                         userId = null;
                     } else {
                         userId = user._id;
@@ -40,41 +41,39 @@ const stream = (req, res) => {
             const view = new View();
             view.viewer = userId;
             view.course = courseId;
-            view.ip_address = ip;
-            view.save(function(err){
-                console.log("view save error", err);
-            });
+            view.ipAddress = ip;
+            view.save();
         
             const path = paths.join(process.cwd() + course.videoUrl);
-            const stat = fs.statSync(path)
-            const fileSize = stat.size
-            const range = req.headers.range
+            const stat = fs.statSync(new String(path));
+            const fileSize = stat.size;
+            const range = req.headers.range;
         
             if (range) {
-                const parts = range.replace(/bytes=/, "").split("-")
-                const start = parseInt(parts[0], 10)
+                const parts = range.replace(/bytes=/, "").split("-");
+                const start = parseInt(parts[0], 10);
                 const end = parts[1]
                 ? parseInt(parts[1], 10)
-                : fileSize-1
+                : fileSize-1;
         
-                const chunksize = (end-start)+1
-                const file = fs.createReadStream(path, {start, end})
+                const chunksize = (end-start)+1;
+                const file = fs.createReadStream(new String(path), {start, end});
                 const head = {
                     "Content-Range": `bytes ${start}-${end}/${fileSize}`,
                     "Accept-Ranges": "bytes",
                     "Content-Length": chunksize,
                     "Content-Type": "video/mp4",
-                }
+                };
         
-                res.writeHead(206, head)
-                file.pipe(res)
+                res.writeHead(206, head);
+                file.pipe(res);
             } else {
                 const head = {
                     "Content-Length": fileSize,
                     "Content-Type": "video/mp4",
-                }
-                res.writeHead(200, head)
-                fs.createReadStream(path).pipe(res)
+                };
+                res.writeHead(200, head);
+                fs.createReadStream(new String(path)).pipe(res);
             }
         }
     });
@@ -87,10 +86,11 @@ const publish = (req, res) => {
     if (token) {
         jwt.verify(token, process.env.JWT_TOKEN || "secret", (err, user) => {
             if (err) {
-                console.log("token error", err);
-                res.status(403).send("Token auth error");
+                res.status(403).json({
+                    message: "Token auth error",
+                    error: err
+                });
             } else {
-                console.log('user', user);
                 if (user.role.toUpperCase() === TEACHER_ROLE) {
                     userId = user._id;
                     if (
@@ -111,8 +111,10 @@ const publish = (req, res) => {
                         course.owner = userId;
                         course.save(function(err, c){
                             if (err) {
-                                console.log("Course Publishing error", err);
-                                res.status(400).send("Course Publishing error");
+                                res.status(400).send({
+                                    message: "Course Publishing error",
+                                    error: err
+                                });
                             }
                             else {
                                 delete c.videoUrl;
@@ -123,7 +125,9 @@ const publish = (req, res) => {
                         return res.status(400).json({ message: "Title, Subject, Language and Video are required." });
                     }
                 } else {
-                    res.status("403").send("Only Teachers can publish");
+                    res.status("403").json({
+                        message: "Only Teachers can publish"
+                    });
                 }
             }
         });
--- models/views.js
+++ models/views.js
@@ -14,7 +14,7 @@ const viewSchema = new mongoose.Schema(
             type: mongoose.Types.ObjectId,
             ref: "Course"
         },
-        ip_address: {
+        ipAddress: {
             type: String,
             trim: true,
         }
