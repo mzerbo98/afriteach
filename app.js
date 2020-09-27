c96af7f567a97e87b6439d5153e6d4310ce82cb3
Jean Cheikh Ndiaye <jeancheikh@gmail.com>
2020-09-27 18:33:33 -0400

    Cors enabled
    adding CORS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var coursesRouter = require("./routes/courses");
@@ -15,6 +15,7 @@ require("./config/passport");
 var app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
